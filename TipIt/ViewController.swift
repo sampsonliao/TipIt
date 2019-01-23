bbf9e8434c2670c378d9ce030972864ca6d28464
Sampson Liao <sampsonliao@sampsons-mac.local>
2019-01-23 15:17:20 -0800

    Initial Commit

---
+++ TipIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipIt
+//
+//  Created by Sampson Liao on 1/23/19.
+//  Copyright © 2019 sliao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
